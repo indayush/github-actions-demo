6fcbc178b2f47771695f0f407e5a88d19f521ad4
indayush <ayush.adarsh@healthgraph.in>
2024-01-25 16:48:37 +0530

    added local ip changes

--- routes/route.js
+++ routes/route.js
@@ -1,9 +1,23 @@
+var os = require('os');
+
 exports.home=function(req,res){
   res.render('home');
+
+  var interfaces = os.networkInterfaces();
+  var addresses = [];
+  for (var k in interfaces) {
+      for (var k2 in interfaces[k]) {
+          var address = interfaces[k][k2];
+          if (address.family === 'IPv4' && !address.internal) {
+              addresses.push(address.address);
+          }
+      }
+  }
+  console.log(addresses);
 }
 
 
-exports.login=function(req,res){ 
+exports.login=function(req,res){
     res.render('login');
 }
 
